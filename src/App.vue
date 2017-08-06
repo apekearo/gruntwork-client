d957b483ea57c62e1c710d3cf61f4e8b038f65a7
Chenglu Li <kabelee92@gmail.com>
2017-08-06 16:56:43 -0500

    Added header nav and drawer

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,71 @@
 <template>
-  <v-app>
-   <!-- <v-layout row wrap child-flex-sm>
-     <v-flex xs5>
-        <v-card dark class="primary">
-          <v-card-text class="display-2 green--text darken-4">Hey Earl</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 offset-xs2>
-       <v-card dark class="primary">
- <v-card-text class="headline green--text darken-4">Login/Signup</v-card-text>
-       </v-card>
-      </v-flex>
-      <v-flex xs12>
-          <v-card-text>A Daylabor Platform</v-card-text>
-      </v-flex>
-      <v-flex xs12 class="display-1"><tabs></tabs></v-flex xs12>
-      <v-flex xs8>
-     <table-grid></table-grid> 
-      </v-flex>
-      <v-flex xs4>
-      <app-map></app-map>
-      </v-flex>
-    </v-layout> -->
-     <!-- <div class="text-xs-center pt-9">
-      <v-pagination v-model="pagination.page" :length="Math.ceil(this.items.length / pagination.rowsPerPage)"></v-pagination>
-    </div>  -->
-    
-  </v-app>
+    <v-app>
+        <app-nav :items="headerItems"></app-nav>
+        <main>
+            <v-container fluid>
+                <div class="title">Click on sidebar to re-open.</div>
+                <!--v-router-->
+            </v-container>
+        </main>
+
+        <!-- <v-layout row wrap child-flex-sm>
+          <v-flex xs5>
+             <v-card dark class="primary">
+               <v-card-text class="display-2 green--text darken-4">Hey Earl</v-card-text>
+             </v-card>
+           </v-flex>
+           <v-flex xs5 offset-xs2>
+            <v-card dark class="primary">
+      <v-card-text class="headline green--text darken-4">Login/Signup</v-card-text>
+            </v-card>
+           </v-flex>
+           <v-flex xs12>
+               <v-card-text>A Daylabor Platform</v-card-text>
+           </v-flex>
+           <v-flex xs12 class="display-1"><tabs></tabs></v-flex xs12>
+           <v-flex xs8>
+          <table-grid></table-grid>
+           </v-flex>
+           <v-flex xs4>
+           <app-map></app-map>
+           </v-flex>
+         </v-layout> -->
+        <!-- <div class="text-xs-center pt-9">
+         <v-pagination v-model="pagination.page" :length="Math.ceil(this.items.length / pagination.rowsPerPage)"></v-pagination>
+       </div>  -->
+
+    </v-app>
 
 
 
 </template>
 
 <script>
-  // import listGroup from './components/JobList/list-group.vue'
-  import map from './components/Map/index.vue'
-  import table from './components/Table/index.vue'
-  import tabs from './components/Table/tabs.vue'
+    // import listGroup from './components/JobList/list-group.vue'
+    import Nav from './components/header.vue';
+    import map from './components/Map/index.vue'
+    import table from './components/Table/index.vue'
+    import tabs from './components/Table/tabs.vue'
 
-  export default {
-    components: {
-      // appListGroup: listGroup,
-      tableGrid: table,
-      appMap: map,
-      tabs: tabs  
-    }}
+    export default {
+        components: {
+            // appListGroup: listGroup,
+            tableGrid: table,
+            appMap: map,
+            tabs: tabs,
+            appNav: Nav
+        },
+        data () {
+            return {
+                headerItems: [
+                    { title: 'Job board', icon: 'dashboard' },
+                    { title: 'Post a job', icon: 'work' }
+                ]
+            }
+        }
+    }
 </script>
 
 <style scoped>
-.headline {display: inline}
+    .headline {display: inline}
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                    <v-list-tile avatar tag="div">
+                        <v-list-tile-avatar>
+                            <img :src='profile' />
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{username}}</v-list-tile-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn icon @click.native.stop="mini = !mini">
+                                <v-icon>chevron_left</v-icon>
+                            </v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <v-list-tile v-for="item in items" :key="item.title">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar fixed class="indigo darken-4" dark>
+            <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+
+<script>
+    import profileImg from '../assets/images/profile.png'
+    export default {
+        data () {
+            return {
+                drawer: false,
+                mini: false,
+                right: null
+            }
+        },
+        props: {
+            title: {
+                type: String,
+                default: 'Hey Earl'
+            },
+            items: {
+                type: Array,
+                default () {
+                    return [
+                        { title: 'Home', icon: 'dashboard' },
+                        { title: 'About', icon: 'question_answer' }
+                    ]
+                }
+            },
+            username: {
+                type: String,
+                default: 'Tony'
+            },
+            profile: {
+                type: String,
+                default: profileImg
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
